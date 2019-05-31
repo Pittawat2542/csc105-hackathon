066371bccd1ee3f22637eb4318ee5b41ff567f32
Michał Zieliński <zielu0@gmail.com>
2019-05-31 14:15:19 +0700

    FIX add new column facebook id to user table

--- config/services.php
+++ config/services.php
@@ -45,13 +45,9 @@ return [
     ],
 
     'facebook' => [
-
         'client_id' => env('FACEBOOK_CLIENT_ID'),
-
         'client_secret' => env('FACEBOOK_CLIENT_SECRET'),
-
         'redirect' => env('FACEBOOK_CALLBACK_URL'),
-
     ],
 
 ];
