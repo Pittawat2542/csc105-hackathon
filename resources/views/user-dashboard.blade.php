bf7e04d12faaf598ae2ebbcc42327a0262d985cb
Pittawat <11158905+Pittawat2542@users.noreply.github.com>
2019-06-01 05:53:40 +0700

    [UPDATE] margin bottom on user dashboard

--- resources/views/user-dashboard.blade.php
+++ resources/views/user-dashboard.blade.php
@@ -2,35 +2,39 @@
 
 @section('content')
     @include('logo')
-    <div class="container">
-        <div class="row">
-            <div class="col-md-6 col-sm-12"><span class="font-weight-bold"><i class="far fa-clock"></i> Total points:</span> {{$raports->count()}}</div>
-            <div class="col-md-6 col-sm-12 text-right"><span class="font-weight-bold"><i class="fas fa-user"></i> {{Auth::user()->name}}</span> has
-                fixed :
+    <section class="mb-3">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 col-sm-12"><span class="font-weight-bold"><i class="far fa-clock"></i> Total points:</span> {{$raports->count()}}
+                </div>
+                <div class="col-md-6 col-sm-12 text-right"><span class="font-weight-bold"><i class="fas fa-user"></i> {{Auth::user()->name}}</span>
+                    has
+                    fixed :
+                </div>
             </div>
         </div>
-    </div>
-    <div class="container">
-        @if($raports)
-            @foreach($raports as $raport)
-                <div class="row">
-                    <div class="card">
-                        <div class="card-body row">
-                            <div class="col-md-4 col-sm-12">
-                                <img class="img-fluid"
-                                     src="{{$raport->photo ? $raport->photo->path: 'https://via.placeholder.com/300'}}"
-                                     alt="">
-                            </div>
-                            <div class="col-md-8 col-sm-12 d-flex flex-column justify-content-around">
-                                <div>
-                                    <p>{{$raport->body}}</p>
-                                    Location: {{$raport->lat}}, {{$raport->lng}} | Added {{$raport->created_at}}
+        <div class="container">
+            @if($raports)
+                @foreach($raports as $raport)
+                    <div class="row">
+                        <div class="card">
+                            <div class="card-body row">
+                                <div class="col-md-4 col-sm-12">
+                                    <img class="img-fluid"
+                                         src="{{$raport->photo ? $raport->photo->path: 'https://via.placeholder.com/300'}}"
+                                         alt="">
+                                </div>
+                                <div class="col-md-8 col-sm-12 d-flex flex-column justify-content-around">
+                                    <div>
+                                        <p>{{$raport->body}}</p>
+                                        Location: {{$raport->lat}}, {{$raport->lng}} | Added {{$raport->created_at}}
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div>
-            @endforeach
-        @endif
-    </div>
+                @endforeach
+            @endif
+        </div>
+    </section>
 @endsection
