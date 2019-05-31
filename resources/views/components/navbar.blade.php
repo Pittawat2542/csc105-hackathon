140c26b8b17be557ce64e84d9b2a9507a92ebd0a
Pittawat <11158905+Pittawat2542@users.noreply.github.com>
2019-05-31 17:57:17 +0700

    [UPDATE] navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-lg bg-primary-orange mb-0 text-white">
     <div class="container">
         <a class="navbar-brand" href="#">
-            BuranaArsa</a>
+            <i class="fas fa-seedling"></i> BuranaArsa</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
                 aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="sr-only">Toggle navigation</span>
@@ -10,6 +10,7 @@
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
+            <div class="mr-auto"></div>
             <ul class="navbar-nav">
                 <li class="nav-item">
                     <a class="nav-link" href="#">Features</a>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,7 +1,8 @@
 @extends('layouts.main')
 
 @section('content')
-    <section>
-    test
+    <section class="mt-3">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium aliquam debitis dicta dolor dolorem
+            dolorum esse eveniet, harum incidunt iure laborum magni nemo odit quasi sequi sint, sit unde voluptatem.</p>
     </section>
 @endsection
