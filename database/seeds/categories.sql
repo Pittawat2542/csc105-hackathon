ca9417629985d7302b0b397dcf3fd360fa529786
Waritnan Sookbuntherng <lion328@hotmail.co.th>
2019-05-31 13:21:17 +0700

    [FIX] Proper Categories seed SQL

--- database/seeds/categories.sql
+++ database/seeds/categories.sql
@@ -3,34 +3,34 @@ INSERT INTO categories (`name`)  VALUES
 ('Ask to dredge the drain.'),
 ('Products that may be dangerous'),
 ('Garbage dropped'),
-'Garbage smelly'
-'Insufficient waste tank'
-'Request to repair, change, add electrical wires and Light bulb'
-'Requesting to install, repair, move, sign, traffic signs'
-'Requesting road repairs'
-'Requesting tree maintenance'
-'Requesting to collect weeds in the canal'
-'Request to organize a messy phone wire'
-'Report the broken pipe / not flow'
-'Tap water does not flow or soft flow'
-'Forsake'
-'Criminals'
-'Student group fleeing school'
-'Child quarrels'
-'Addictive substance'
-'Alcohol'
-'Nomadic person'
-'Requesting the collection of dead animals'
-'Found a venomous snake or varanus'
-'House pillars, buildings, damaged scaffolding'
-'chemicals'
-'Drainage into the streets or houses of others'
-'Requesting action with dogs, rats, mosquitoes, pigeons'
-'the sound'
-'Collapsed building'
-'Collect weapons, transfer weapons'
-'Accident'
-'Security reason'
-'Fire incident'
-'rally or rioting'
-'Explosion incident'
+('Garbage smelly'),
+('Insufficient waste tank'),
+('Request to repair, change, add electrical wires and Light bulb'),
+('Requesting to install, repair, move, sign, traffic signs'),
+('Requesting road repairs'),
+('Requesting tree maintenance'),
+('Requesting to collect weeds in the canal'),
+('Request to organize a messy phone wire'),
+('Report the broken pipe / not flow'),
+('Tap water does not flow or soft flow'),
+('Forsake'),
+('Criminals'),
+('Student group fleeing school'),
+('Child quarrels'),
+('Addictive substance'),
+('Alcohol'),
+('Nomadic person'),
+('Requesting the collection of dead animals'),
+('Found a venomous snake or varanus'),
+('House pillars, buildings, damaged scaffolding'),
+('chemicals'),
+('Drainage into the streets or houses of others'),
+('Requesting action with dogs, rats, mosquitoes, pigeons'),
+('the sound'),
+('Collapsed building'),
+('Collect weapons, transfer weapons'),
+('Accident'),
+('Security reason'),
+('Fire incident'),
+('rally or rioting'),
+('Explosion incident')
