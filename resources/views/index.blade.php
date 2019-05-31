ce966d6a15ae59302cfd947b73736a1e65b7e294
Wicharn <wicharn.keisei@gmail.com>
2019-05-31 22:08:59 +0700

    [update] fix index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,7 +4,7 @@
     @include('logo')
     <div class="container">
         <div class="row justify-content-center">
-            <a class="col-auto btn btn-danger btn-raised my-3" style="font-size: 1rem;" href="{{ route('create.raport') }}">
+            <a class="col-auto btn btn-danger btn-raised my-3" style="font-size: 1rem;" href="{{ route('create.raport') }}">REPORT</a>
         </div>
         <div class="row">
             <div id="category" class="col">
