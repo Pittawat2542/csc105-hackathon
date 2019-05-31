ef309c59454526560d7e1f5d6af2700d81b1d4f3
Wicharn <wicharn.keisei@gmail.com>
2019-05-31 20:52:38 +0700

    [add] user-dashboard and update volunteer rank

---
+++ resources/views/user-dashboard.blade.php
@@ -0,0 +1,62 @@
+@extends('layouts.main')
+
+@section('content')
+    @include('logo')
+    <div class="container">
+        <div>
+            <div class="container text-center">
+                <p class="display-4">Username</p>
+                <p class="display-4">has fixed :</p>
+            </div>
+            <div class="card p-5">
+                <div class="row">
+                    <div class="col-auto mb-2">
+                        <img class="report-img" src="https://ichef.bbci.co.uk/news/976/media/images/83351000/jpg/_83351965_explorer273lincolnshirewoldssouthpicturebynicholassilkstone.jpg"
+                             alt="" >
+                    </div>
+                    <div class="col">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab debitis dolor doloremque
+                            explicabo laborum natus non perspiciatis placeat quaerat, quasi quis reiciendis, soluta
+                            tempore vitae voluptate! Consequatur debitis laborum maiores?</p>
+                    </div>
+                </div>
+            </div>
+            <div class="card p-5">
+                <div class="row">
+                    <div class="col-auto mb-2">
+                        <img class="report-img" src="https://ichef.bbci.co.uk/news/976/media/images/83351000/jpg/_83351965_explorer273lincolnshirewoldssouthpicturebynicholassilkstone.jpg"
+                             alt="" >
+                    </div>
+                    <div class="col">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab debitis dolor doloremque
+                            explicabo laborum natus non perspiciatis placeat quaerat, quasi quis reiciendis, soluta
+                            tempore vitae voluptate! Consequatur debitis laborum maiores?</p>
+                    </div>
+                </div>
+            </div>
+            <div class="card p-5">
+                <div class="row">
+                    <div class="col-auto mb-2">
+                        <img class="report-img" src="https://ichef.bbci.co.uk/news/976/media/images/83351000/jpg/_83351965_explorer273lincolnshirewoldssouthpicturebynicholassilkstone.jpg"
+                             alt="" >
+                    </div>
+                    <div class="col">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab debitis dolor doloremque
+                            explicabo laborum natus non perspiciatis placeat quaerat, quasi quis reiciendis, soluta
+                            tempore vitae voluptate! Consequatur debitis laborum maiores?</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <style>
+        .report-img{
+            width: 150px;
+        }
+        @media screen and (max-width: 568px){
+            .report-img{
+                /*width: 150px;*/
+            }
+        }
+    </style>
+@endsection
