487abf631ea7a2fc3394e0347b7de4875898ffaf
Wicharn <wicharn.keisei@gmail.com>
2019-06-01 15:41:19 +0700

    [update] ranking if same point

--- resources/views/volunteer-ranking.blade.php
+++ resources/views/volunteer-ranking.blade.php
@@ -9,13 +9,13 @@
             <div class="card">
                 <div class="card-footer">
                     <div class="mr-5 ml-3">
-                        <i class="fas fa-medal trophy-icon d-block m-0"></i>
+                        <i class="fas fa-medal trophy-icon d-block m-0 rank"></i>
                         <img src="https://randomuser.me/api/portraits/men/55.jpg" class="user-img" alt="user image">
-                        <h4 class="d-hidden card-h4" id="first"></h4>
+                        <h4 class="card-h4 rank" id="first"></h4>
                     </div>
                     <div class="text-left">
                         <p class="h3 mt-0 font-weight-bold">{{$rank['user']->name}}</p>
-                        <p class="h4"><span class="font-weight-bold">Volunteer Point : </span>{{$rank['value']}}</p>
+                        <p class="h4 user-point"><span class="font-weight-bold">Volunteer Point : </span>{{$rank['value']}}</p>
                     </div>
                 </div>
             </div>
@@ -24,14 +24,78 @@
 
     <script>
         $(document).width();
-        var first = document.querySelector('#first');
 
-        vltl = document.querySelectorAll('#volunteerList h4');
-        if (vltl[0]) vltl[0].innerHTML = '1<sup>st</sup>'
-        if (vltl[1]) vltl[1].innerHTML = '2<sup>nd</sup>'
-        if (vltl[2]) vltl[2].innerHTML = '3<sup>rd</sup>'
-        for (i = 3; i < vltl.length; ++i) {
-            vltl[i].innerHTML = `${i + 1}<sup>th</sup>`
+
+
+        vltl = document.querySelectorAll('h4.rank');
+        vltl2 = document.querySelectorAll('i.rank');
+        vltl3 = document.querySelectorAll('#volunteerList .user-point');
+        // for(var key in vltl3){
+        //     console.log(vltl3[key].innerHTML);
+        // }
+        for (i = 0,currentRank = 1,lastPoint =vltl3[0].innerHTML.substring(56); i < vltl.length; ++i) {
+            var thisPoint = vltl3[i].innerHTML.substring(56);
+            console.log(i);
+            if(i==0){
+                vltl[i].classList.add('rank-1');
+                vltl2[i].classList.add('rank-1');
+                vltl[0].innerHTML = '1<sup>st</sup>'
+                lastPoint =thisPoint;
+                console.log(vltl[i].classList);
+            }else{
+                if(currentRank==1){
+                    if(lastPoint==thisPoint){
+                        vltl[i].classList.add('rank-1');
+                        vltl2[i].classList.add('rank-1');
+                        vltl[i].innerHTML = '1<sup>st</sup>'
+                    }else{
+                        vltl[i].classList.add('rank-2');
+                        vltl2[i].classList.add('rank-2');
+                        vltl[i].innerHTML = '2<sup>nd</sup>'
+                        lastPoint = thisPoint;
+                        currentRank =i;
+                    }
+                }else if(currentRank == 2){
+                    if(lastPoint==thisPoint){
+                        vltl[i].classList.add('rank-2');
+                        vltl2[i].classList.add('rank-2');
+                        vltl[i].innerHTML = '2<sup>nd</sup>'
+                    }else{
+                        vltl[i].classList.add('rank-3');
+                        vltl2[i].classList.add('rank-3');
+                        vltl[i].innerHTML = '3<sup>rd</sup>'
+                        lastPoint = thisPoint;
+                        currentRank =i;
+                    }
+                }
+                else if(currentRank == 3){
+                    if(lastPoint==thisPoint){
+                        vltl[i].classList.add('rank-3');
+                        vltl2[i].classList.add('rank-3');
+                        vltl[i].innerHTML = '2<sup>nd</sup>'
+                    }else{
+                        vltl[i].innerHTML =i+'<sup>th</sup>'
+                        lastPoint = thisPoint;
+                        currentRank =i;
+                    }
+                }else {
+                    if(lastPoint==thisPoint){
+                        vltl[i].innerHTML = '2<sup>nd</sup>'
+                    }else{
+                        vltl[i].innerHTML =i+'<sup>th</sup>'
+                        lastPoint = thisPoint;
+                        currentRank =i;
+                    }
+                }
+                console.log('curr:',currentRank,'i:',i,'last:',lastPoint)
+            }
+
+            // if(currentRank==1&&lastPoint)
+            //
+            // vltl[i].innerHTML = `${i + 1}<sup>th</sup>`
+            // if (vltl[0]) vltl[0].innerHTML = '1<sup>st</sup>'
+            // if (vltl[1]) vltl[1].innerHTML = '2<sup>nd</sup>'
+            // if (vltl[2]) vltl[2].innerHTML = '3<sup>rd</sup>'
         }
 
     </script>
