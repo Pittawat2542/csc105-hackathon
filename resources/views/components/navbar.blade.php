be1a2f7e2469d345a466bfbf96b22bcdb7d3a87b
Pittawat <11158905+Pittawat2542@users.noreply.github.com>
2019-06-01 19:00:50 +0700

    [UPDATE] :lipstick: space between logout button

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -25,20 +25,23 @@
                 @endguest
 
                 @auth
+                    <li class="nav-item">
+                        <a href="{{Route('index.raport')}}" class="nav-link">Welcome, <i
+                                class="fas fa-user"></i> {{ Auth::user()->name }}</a>
+                    </li>
+                    @if(Auth::user()->isAdmin())
                         <li class="nav-item">
-                            <a href="{{Route('index.raport')}}" class="nav-link">Welcome, <i class="fas fa-user"></i> {{ Auth::user()->name }}</a>
-                        </li>
-                        @if(Auth::user()->isAdmin())
-                            <li class="nav-item">
                             <a href="{{route('admin')}}" class="nav-link"><i class="fas fa-tools"></i> Admin Panel</a>
                         </li>
-                        @endif
-                        <form action="/logout" method="POST">
-                            @csrf
-                            <li class="nav-item">
-                                <button type="submit" class="btn btn-danger my-2 my-sm-0"><i class="far fa-times-circle"></i> Logout</button>
-                            </li>
-                        </form>
+                    @endif
+                    <form action="/logout" method="POST" class="ml-1">
+                        @csrf
+                        <li class="nav-item">
+                            <button type="submit" class="btn btn-danger my-2 my-sm-0"><i
+                                    class="far fa-times-circle"></i> Logout
+                            </button>
+                        </li>
+                    </form>
                 @endauth
             </ul>
         </div>
