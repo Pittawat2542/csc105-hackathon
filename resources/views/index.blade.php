6fee829598b7abc3cd0b6a4b46a7123b7a59de13
Pittawat <11158905+Pittawat2542@users.noreply.github.com>
2019-06-01 19:30:52 +0700

    Try geolocation

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -67,66 +67,20 @@
         var lat;
         var lng;
 
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(function (position) {
-                lat = position.coords.latitude;
-                lng = position.coords.longitude;
-
-                $.ajax({
-                    url: '/getgeo',
-                    data: {latitude: lat, longitude: lng, _method: 'GET'},
-                    type: "POST",
-
-                    success: function (data) {
-                    }
-
-                });
-            });
-        }
-
-        var apiGeolocationSuccess = function(position) {
-            alert("API geolocation success!\n\nlat = " + position.coords.latitude + "\nlng = " + position.coords.longitude);
+        var apiGeolocationSuccess = function (position) {
+            lat = position.coords.latitude;
+            lng = position.coords.longitude;
         };
 
-        var tryAPIGeolocation = function() {
-            jQuery.post( "https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyB5pqlf37NqcN8xW6-FW2pbFEgpZ7ssTIk", function(success) {
+        var tryAPIGeolocation = function () {
+            jQuery.post("https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyB5pqlf37NqcN8xW6-FW2pbFEgpZ7ssTIk", function (success) {
                 apiGeolocationSuccess({coords: {latitude: success.location.lat, longitude: success.location.lng}});
             })
-                .fail(function(err) {
-                    alert("API Geolocation error! \n\n"+err);
+                .fail(function (err) {
                 });
         };
 
-        var browserGeolocationSuccess = function(position) {
-            alert("Browser geolocation success!\n\nlat = " + position.coords.latitude + "\nlng = " + position.coords.longitude);
-        };
-
-        var browserGeolocationFail = function(error) {
-            switch (error.code) {
-                case error.TIMEOUT:
-                    alert("Browser geolocation error !\n\nTimeout.");
-                    break;
-                case error.PERMISSION_DENIED:
-                    if(error.message.indexOf("Only secure origins are allowed") == 0) {
-                        tryAPIGeolocation();
-                    }
-                    break;
-                case error.POSITION_UNAVAILABLE:
-                    alert("Browser geolocation error !\n\nPosition unavailable.");
-                    break;
-            }
-        };
-
-        var tryGeolocation = function() {
-            if (navigator.geolocation) {
-                navigator.geolocation.getCurrentPosition(
-                    browserGeolocationSuccess,
-                    browserGeolocationFail,
-                    {maximumAge: 50000, timeout: 20000, enableHighAccuracy: true});
-            }
-        };
-
-        tryGeolocation();
+        tryAPIGeolocation();
 
         var locationObj = {lat: parseFloat(lat), lng: parseFloat(lng)};
 
